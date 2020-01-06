29f2a33366e94a386ad8b77657125defabd8d730
7077 <7077@mait-13-05.local>
2020-01-06 12:59:30 +1100

    Initial commit

---
+++ Coloniam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coloniam
+//
+//  Created by 7077 on 6/1/20.
+//  Copyright © 2020 7077. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ColoniamTests/ColoniamTests.swift
@@ -0,0 +1,34 @@
+//
+//  ColoniamTests.swift
+//  ColoniamTests
+//
+//  Created by 7077 on 6/1/20.
+//  Copyright © 2020 7077. All rights reserved.
+//
+
+import XCTest
+@testable import Coloniam
+
+class ColoniamTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
